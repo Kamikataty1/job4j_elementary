ac883c59dfd20d0d8d206ce454b3a870be315020
kamikataty1 <tryapitsyn.se@yandex.ru>
2025-05-14 23:54:53 +0300

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,16 +2,16 @@ package ru.job4j.math;
 
 public class MathFunction {
 
-    public static double multiply(double first, double second) {
+    public static double multiply(double first,double second) {
         return first * second;
     }
-    public static double delen(double first, double second) {
+    public static double delen(double first,double second) {
         return first / second;
     }
-    public static double razn(double first, double second) {
+    public static double razn(double first,double second) {
         return first - second;
     }
-    public static double plus(double first, double second) {
+    public static double plus(double first,double second) {
         return first + second;
     }
 }
