01b1e00cb0270a61968edfd03217a64d3df6d11c
kamikataty1 <tryapitsyn.se@yandex.ru>
2025-12-18 16:14:28 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -13,7 +13,5 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
-        System.out.println(sum(2, 6));
-        System.out.println(sum(5, 3));
     }
 }
