b0f370454e725b4f65fc368992d4c01479ed8b7d
kamikataty1 <tryapitsyn.se@yandex.ru>
2025-12-23 20:26:47 +0300

    Протеиновая диета (цикл while)

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (month < 100);
+        return month;
+    }
+}
